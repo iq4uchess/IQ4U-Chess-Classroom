19c292ec9422c0157b54b6eab8d313bd6a75c104
iq4uchess <iq4uchess@gmail.com>
2025-12-21 15:49:33 +0530

    Update engine.js

--- js/stockfish/Stockfish-17.1-.js-.wasm-.nnue-main/engine.js
+++ js/stockfish/Stockfish-17.1-.js-.wasm-.nnue-main/engine.js
@@ -1,3 +1,13 @@
+// Tell Stockfish where the WASM file lives
+self.Module = {
+  locateFile: (path) => {
+    if (path.endsWith('.wasm')) {
+      return './stockfish-17.1.wasm';
+    }
+    return path;
+  }
+};
+
 importScripts('./stockfish-17.1.js');
 
 self.onmessage = (e) => {
