6726d833cfa5279c5dbfda8f139507c538a2c7e5
iq4uchess <iq4uchess@gmail.com>
2025-12-30 18:20:52 +0530

    Update vite.config.js

--- React app/IQ 4U Class Room/vite.config.js
+++ React app/IQ 4U Class Room/vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/IQ4U-Chess-Classroom/react-app/",
   plugins: [react()]
 });
