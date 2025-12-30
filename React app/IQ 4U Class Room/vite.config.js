5880707f0ec1a9d0fad7f73b0f81358f82227ca9
iq4uchess <iq4uchess@gmail.com>
2025-12-30 18:36:53 +0530

    Delete React app/IQ 4U Class Room directory

--- React app/IQ 4U Class Room/vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-export default defineConfig({
-  base: "/IQ4U-Chess-Classroom/react-app/",
-  plugins: [react()]
-});
